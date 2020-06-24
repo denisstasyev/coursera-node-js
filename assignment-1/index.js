dc4ca98f2dbd3e5e2e93d1183f4c8e29a99fe9ef
Stasyev Denis <d.stasyev@corp.mail.ru>
2020-06-24 23:59:13 +0300

    Add dishRouter

---
+++ assignment-1/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const dishRouter = require('./routes/dishRouter')
+// const promoRouter = require('./routes/promoRouter')
+// const leaderRouter = require('./routes/leaderRouter')
+
+const hostname = 'localhost'
+const port = 3000
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.use('/dishes', dishRouter)
+// app.use('/promotions', promoRouter)
+// app.use('/leadership', leaderRouter)
+
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+	res.statusCode = 404
+	res.setHeader('Content-Type', 'text/html')
+	res.end('<html><body><h1>Error 404 - Not Found</h1></body></html>')
+})
+
+app.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`)
+})
